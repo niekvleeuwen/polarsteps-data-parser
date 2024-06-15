550c928ed925eed47e5d1c94b2e997d6bd984651
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2024-06-15 22:52:45 +0200

    Refactored PDF generator

--- polarsteps_data_parser/pdf_generator.py
+++ polarsteps_data_parser/pdf_generator.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 from loguru import logger
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.utils import ImageReader
+from reportlab.pdfbase.pdfmetrics import stringWidth
 from reportlab.pdfgen.canvas import Canvas
 
 from polarsteps_data_parser.model import Trip, Step
@@ -10,24 +13,53 @@ class PDFGenerator:
     """Generates a PDF for Polarsteps Trip objects."""
 
     PHOTO_WIDTH = 250
+    MAIN_FONT = ("Helvetica", 12)
+    BOLD_FONT = ("Helvetica-Bold", 12)
+    HEADING_FONT = ("Helvetica-Bold", 16)
+    TITLE_HEADING_FONT = ("Helvetica-Bold", 36)
 
     def __init__(self, output: str) -> None:
         self.filename = output
         self.canvas = None
-        self.y_position = None
-        self.width = None
-        self.height = None
+        self.width, self.height = letter
+        self.y_position = self.height - 30
 
     def generate_pdf(self, trip: Trip) -> None:
         """Generate a PDF for a given trip."""
         self.canvas = Canvas(self.filename, pagesize=letter)
 
+        self.canvas.setTitle(trip.name)
+
+        self.generate_title_page(trip)
+
         no_of_steps = len(trip.steps)
         for i, step in enumerate(trip.steps[:2], start=1):
             logger.debug(f"{i}/{no_of_steps} generating pages for step {step.name}")
-            self.create_step_pdf(step)
+            self.generate_step_pages(step)
 
         self.canvas.save()
+
+    def generate_title_page(self, trip: Trip) -> None:
+        """Generate title page."""
+        self.title_heading(trip.name)
+
+    def generate_step_pages(self, step: Step) -> None:
+        """Add a step to the canvas."""
+        self.new_page()
+
+        self.heading(step.name)
+
+        self.short_text(f"Location: {step.location.name}, {step.location.country}")
+        self.short_text(f"Date: {step.date.strftime('%d-%m-%Y')}")
+
+        self.long_text(step.description)
+
+        for comment in step.comments:
+            self.short_text(comment.follower.name, bold=True)
+            self.long_text(comment.text)
+
+        for photo in step.photos:
+            self.photo(photo)
 
     def new_page(self) -> None:
         """Add a new page to the canvas."""
@@ -35,60 +67,48 @@ class PDFGenerator:
         self.width, self.height = letter
         self.y_position = self.height - 30
 
-    def create_step_pdf(self, step: Step) -> None:
-        """Add a step to the canvas."""
-        self.new_page()
-
-        # Title
-        self.heading(step.name)
-
-        # Basic Information
-        self.short_text(f"Location: {step.location.name}, {step.location.country}")
-        self.short_text(f"Date: {step.date.strftime('%d-%m-%Y')}")
-
-        # Description
-        self.long_text(step.description)
-
-        # Comments
-        self.long_text("Comments:")
-        for comment in step.comments:
-            self.short_text(f"Author: {comment.follower.name}")
-            self.long_text(comment.text)
-
-        # Photos
-        self.short_text("Photos:")
-        for photo in step.photos:
-            self.photo(photo)
-
     def heading(self, text: str) -> None:
         """Add heading to canvas."""
         if self.y_position < 50:
             self.new_page()
-        self.canvas.setFont("Helvetica-Bold", 16)
+        self.canvas.setFont(*self.HEADING_FONT)
         self.canvas.drawString(30, self.y_position, text)
         self.y_position -= 30
 
-    def short_text(self, text: str) -> None:
+    def title_heading(self, text: str) -> None:
+        """Add heading to canvas."""
+        self.y_position -= 100
+        self.canvas.setFont(*self.TITLE_HEADING_FONT)
+        self.canvas.drawString(self.calc_width_centered(text, self.TITLE_HEADING_FONT), self.y_position, text)
+        self.y_position -= 30
+
+    def calc_width_centered(self, text: str, font: tuple) -> float:
+        """Calculate the width location to center the text."""
+        return (self.width - stringWidth(text, *font)) / 2.0
+
+    def short_text(self, text: str, bold: bool = False, centered: bool = False) -> None:
         """Add short text to canvas."""
         if self.y_position < 50:
             self.new_page()
-        self.canvas.setFont("Helvetica", 12)
-        self.canvas.drawString(30, self.y_position, text)
+        font = self.BOLD_FONT if bold else self.MAIN_FONT
+        self.canvas.setFont(*font)
+        width = self.calc_width_centered(text, self.MAIN_FONT) if centered else 30
+        self.canvas.drawString(width, self.y_position, text)
         self.y_position -= 20
 
     def long_text(self, text: str) -> None:
         """Add long text to canvas."""
         self.y_position -= 10
-        lines = self.wrap_text(text, self.width - 60, self.canvas, "Helvetica", 12)
+        lines = self.wrap_text(text, self.width - 60)
         for line in lines:
             if self.y_position < 50:
                 self.new_page()
-                self.canvas.setFont("Helvetica", 12)
+                self.canvas.setFont(*self.MAIN_FONT)
             self.canvas.drawString(30, self.y_position, line)
             self.y_position -= 20
         self.y_position -= 20
 
-    def photo(self, photo_path: str) -> None:
+    def photo(self, photo_path: Path) -> None:
         """Add photo to canvas."""
         try:
             image = ImageReader(photo_path)
@@ -109,16 +129,15 @@ class PDFGenerator:
         except Exception as e:
             logger.warning(f"Failed to load image {photo_path}: {e}")
 
-    @staticmethod
-    def wrap_text(text: str, max_width: int, canvas: Canvas, font: str, font_size: str) -> list:
+    def wrap_text(self, text: str, max_width: int) -> list:
         """Wrap text to fit within max_width."""
-        canvas.setFont(font, font_size)
+        self.canvas.setFont(*self.MAIN_FONT)
         lines = []
         words = text.split()
         current_line = ""
         for word in words:
             test_line = f"{current_line} {word}".strip()
-            if canvas.stringWidth(test_line) <= max_width:
+            if self.canvas.stringWidth(test_line) <= max_width:
                 current_line = test_line
             else:
                 lines.append(current_line)
